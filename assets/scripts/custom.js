955ff933249c425358d32971518943d8d237565a
vilted <42766290+vilted@users.noreply.github.com>
2018-09-23 23:58:28 +0300

    Add files via upload

---
+++ assets/scripts/custom.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+
+	// burger menu
+	$('.burger').click(function(){
+		
+		$('.nav').toggleClass('show');
+	
+	});
+
+
+	// smooth scrolling
+
+	$('a[href^="#"]').click(function(){
+
+	var the_id = $(this).attr("href");
+
+	    $('html, body').animate({
+	        scrollTop:$(the_id).offset().top
+	    }, 'slow');
+
+	return false;
+	});
+
+}); // ready function closed
+
+
+
+
+
+
+
+
+
