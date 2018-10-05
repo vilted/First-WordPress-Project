03e1080de3d934602fd65a486538fe4ad1ba1c05
vilted <42766290+vilted@users.noreply.github.com>
2018-10-06 00:19:19 +0300

    Add files via upload

---
+++ partials/about.php
@@ -0,0 +1,35 @@
+<section id="ABOUT" class="about"> <!-- main block -->
+    <div class="container"> <!-- container max-width 1200px -->
+        <div class="about-heading"> <!-- short about description -->
+            <h3><?php the_field('ac_heading'); ?></h3>
+            <p><?php the_field('ac_about_text'); ?></p>
+        </div>
+        <div id="ABOUT2" class="columns flex-container"> <!-- about details -->
+            <?php
+            if(have_rows('ac_one_service_repeater')):
+                while(have_rows('ac_one_service_repeater')): the_row();
+                    ?>
+                    <div class="column flex-container">
+                        <div class="icon">
+                            <?php 
+                                $image = get_sub_field('ac_service_icon');
+                                if( !empty($image) ): ?>
+                                    <img src="<?php echo $image['sizes']['icon']; ?>" alt="<?php echo $image['alt']; ?>" />
+                                    <?php 
+                                endif;
+                            ?>
+                        </div>
+                        <h4 class="name"><?php the_sub_field('ac_service_name'); ?></h4>
+                        <p><?php the_sub_field('ac_service_description') ?></p> 
+                    </div>
+                    <?php
+                endwhile;
+            endif;
+            ?>
+        </div>
+    </div>
+</section> <!-- main block closed -->
+<div class="container"> <!-- container max-width 1200px -->
+    <div class="break"></div>
+</div>
+
---
+++ partials/social_networks.php
@@ -0,0 +1,48 @@
+<section class="extra-contacts"> <!-- main block -->
+    <div class="container"> <!-- container max-width 1200px -->
+        <div class="social-networks flex-container"> <!-- social networks info -->
+            <?php
+            if(have_rows('snm_social_network_navigation_repeater')):
+                while(have_rows('snm_social_network_navigation_repeater')): 
+                    the_row();
+                    $link = get_sub_field('social_network_link');
+                    if($link['target']=="_blank"){
+                        $link['target']='target="_blank"';
+                    }else{
+                        $link['target']='';
+                    }
+                    ?>
+                    <div class="transparent-frames flex-container">
+                        <div class="frames flex-container">
+                            <a href="<?php echo $link['url'] ?>" <?php echo $link['target']; ?>>
+                                <?php 
+                                $image = get_sub_field('social_network_icon');
+                                    if( !empty($image) ): ?>
+                                    <img src="<?php echo $image['sizes']['icon-social']; ?>" alt="<?php echo $image['alt']; ?>" />
+                                    <?php 
+                                endif;
+                                ?>    
+                           </a>
+                        </div>
+                    </div>
+                    <?php
+                endwhile;
+            endif;
+            ?>
+        </div>
+        <div class="author flex-container"> <!-- credits -->
+            <h4><?php the_field('snmac_copyright'); ?></h4>
+            <h4><?php 
+            if(get_field('snmac_imprint')):
+                the_field('snmac_imprint_text');
+            else:
+                $image = get_field('snmac_imprint_image');
+                if( !empty($image) ): ?>
+                    <img src="<?php echo $image['sizes']['thumbnail']; ?>" alt="<?php echo $image['alt']; ?>" />
+                    <?php
+                endif;
+            endif;
+            ?></h4>
+        </div>
+    </div>
+</section> <!-- main block closed -->
